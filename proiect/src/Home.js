216ff7ade564dba84172314c725325a00fb57f88
DariusPvl3 <paval.darius26@gmail.com>
2023-05-04 15:14:14 +0300

    Navbar stage 1

---
+++ proiect/src/Home.js
@@ -0,0 +1,14 @@
+import './styles/Home.css';
+import React from 'react';
+import Navbar from './Navbar';
+
+function Home() {
+  return (
+    <div className="Home">
+      <Navbar />
+      {/* Add other components here */}
+    </div>
+  );
+}
+
+export default Home;
