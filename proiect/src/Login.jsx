83bad5335f98d04736f1ccf64376b62545df7cec
DariusPvl3 <paval.darius26@gmail.com>
2023-05-09 01:12:54 +0300

    git commit

--- proiect/src/Login.jsx
+++ proiect/src/Login.jsx
@@ -2,34 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
-    async function handleSubmit(e){
-        e.preventDefault();
-
-        try {
-            await axios.post("http://localhost:3000/", {
-                email, pass
-            })
-        } 
-        catch(e){
-            console.log(e);
-        }
-    }
-
+function Login() {
     return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit" onClick={handleSubmit} className="btn">Log In</button>
+        <div className="login">
+            <h1>Log In</h1>
+            <form action="POST">
+                <input type="text" onChange={(e)=>{
+
+                }} name= "" id= "" />
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
     )
 }
