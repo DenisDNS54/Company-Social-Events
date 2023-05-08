4c0c6a08b4ddc461a4ef8c7f13e8ee68522838de
DenisDNS54 <popovicidenisioan@gmail.com>
2023-05-09 01:11:20 +0300

    git commit

--- proiect/src/App.jsx
+++ proiect/src/App.jsx
@@ -4,19 +4,23 @@ import { Login } from "./Login";
 import { Register } from "./Register";
 //nou>
 //import 
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
 
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
