66a4418bf937456d74aadbeb053178dcc99c0db2
DariusPvl3 <paval.darius26@gmail.com>
2023-05-08 19:39:39 +0300

    calendar with adding events working but not saved

--- proiect/src/components/Navbar.jsx
+++ proiect/src/components/Navbar.jsx
@@ -1,31 +1,52 @@
-import '../styles/Navbar.css';
-import React from 'react';
+import './styles/Navbar.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleButtonClick = () => {
+    setOpen(!open);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
   return (
-    <div class="navbar">
-      <div class="left-side">
+    <div class ="navbar">
+      <div class ="left-side">
         <a href="Home.js">Logo</a>
       </div>
-      <div className="center-side">
+      <div class ="center-side">
         <a href="#news">Week</a>
         <a href="#news">Month</a>
       </div>
-      <div className="right-side">
-        <div class="dropdown">
-          <button class="dropbtn">Account
-            <i class="fa fa-caret-down"></i>
+      <div class ="right-side">
+        <div class ="dropdown" ref={dropdownRef}>
+          <button type="button" class ="dropbtn" onClick={handleButtonClick}>
+            Account <i class ="fa fa-caret-down"></i>
           </button>
-          <div class="dropdown-content">
-            <a href="#">Account</a>
-            <a href="#">Settings</a>
-            <a href="#">Log Out</a>
-          </div>
+          {open && (
+            <div class ="dropdown-content">
+              <li><a href="#">Account</a></li>
+              <li><a href="#">Settings</a></li>
+              <li><a href="#">Log Out</a></li>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 }
-
 
 export default Navbar;
