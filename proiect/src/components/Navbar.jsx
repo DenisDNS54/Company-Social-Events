620859942710ee240d00a9484bdd42b7e98aa5f6
DariusPvl3 <paval.darius26@gmail.com>
2023-05-08 22:37:28 +0300

    added color palette

--- proiect/src/components/Navbar.jsx
+++ proiect/src/components/Navbar.jsx
@@ -1,26 +1,7 @@
 import '../styles/Navbar.css';
-import React, { useState, useRef, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
-  const dropdownRef = useRef(null);
-
-  const handleButtonClick = () => {
-    setOpen(!open);
-  };
-
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
-    return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  }, []);
 
   return (
     <div className ="navbar">
