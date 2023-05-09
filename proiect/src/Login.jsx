308c68e1bef28477d3d428fc74257e4aca183029
DenisDNS54 <popovicidenisioan@gmail.com>
2023-05-09 13:41:28 +0300

    css si modificari

--- proiect/src/Login.jsx
+++ proiect/src/Login.jsx
@@ -38,8 +38,8 @@ function Login() {
         <div className="login">
             <h1>Log In</h1>
             <form action="POST">
-                <input type="email" onChange={(e) => { setEmail(e.target.value) }} name="Email" id="" />
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} name="Password" id="" />
+                <input type="email" onChange={(e) => { setEmail(e.target.value) }} name="Email" id="" /><br></br>
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} name="Password" id="" /><br></br>
 
                 <input type="submit" onClick={submit}/>
             </form>
