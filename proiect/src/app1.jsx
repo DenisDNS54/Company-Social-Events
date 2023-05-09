b429d426592a562e39c40147a3ab4fdf05f6d534
DenisDNS54 <popovicidenisioan@gmail.com>
2023-05-09 11:25:26 +0300

    connexiune

--- proiect/src/app1.jsx
+++ proiect/src/app1.jsx
@@ -1,6 +1,19 @@
 const express = require("express")
 const collection = require("./mongo")
 const cors = require("cors")
+
+const mongoose = require("mongoose");
+
+const mongoUrl="mongodb+srv://popovicidenis:parola@cluster0.t5wdhkq.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUrl,{
+  useNewUrlParser:true
+})
+.then(()=>{
+  console.log("Connected to database");
+})
+.catch((e)=> console.log(e));
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
