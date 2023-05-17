ead9269dae7c1f3023a1d7b510652557fe6cf53a
DariusPvl3 <paval.darius26@gmail.com>
2023-05-18 00:47:56 +0300

    Styled Calendar a bit

--- proiect/src/components/Calendar.jsx
+++ proiect/src/components/Calendar.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 function Calendar() {
+  const calendarRef = useRef(null);
   const [calendarEvents, setCalendarEvents] = useState([]);
 
   const handleEventAdd = ({ event }) => {
     if (event && event.title && event.start) {
-        setCalendarEvents([...calendarEvents, event]);
+      setCalendarEvents([...calendarEvents, event]);
     }
   };
 
@@ -26,35 +27,74 @@ function Calendar() {
     }
   };
 
+  const handleTodayButtonClick = () => {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.today();
+  };
+
+  const handleDayGridMonthButtonClick = () => {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.changeView("dayGridMonth");
+  };
+
+  const handleTimeGridWeekButtonClick = () => {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.changeView("timeGridWeek");
+  };
+
+  const handleTimeGridDayButtonClick = () => {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.changeView("timeGridDay");
+  };
+
   const handleDateRangeChange = (rangeInfo) => {
     const { startStr, endStr } = rangeInfo;
     const newEvents = [];
-  
+
     // TODO: Fetch events that fall within the new date range
-  
+
     setCalendarEvents(newEvents);
   };
 
   return (
     <div>
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView={"dayGridMonth"}
-            headerToolbar={{
-                start: "today prev,next",
-                center: "",
-                end: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            height={"90vh"}
-            events={calendarEvents}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={true}
-            datesSet={handleDateRangeChange}
-            select={handleDateSelect}
-            eventAdd={handleEventAdd}
-        />
+      <FullCalendar
+        ref={calendarRef}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        customButtons={{
+          customTodayButton: {
+            text: "Today",
+            click: handleTodayButtonClick,
+          },
+          customDayGridMonthButton: {
+            text: "Month",
+            click: handleDayGridMonthButtonClick,
+          },
+          customTimeGridWeekButton: {
+            text: "Week",
+            click: handleTimeGridWeekButtonClick,
+          },
+          customTimeGridDayButton: {
+            text: "Day",
+            click: handleTimeGridDayButtonClick,
+          },
+        }}
+        headerToolbar={{
+          start: "customTodayButton prev next",
+          center: "title",
+          end: "customTimeGridDayButton customTimeGridWeekButton customDayGridMonthButton",
+        }}
+        height={"90vh"}
+        events={calendarEvents}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={true}
+        datesSet={handleDateRangeChange}
+        select={handleDateSelect}
+        eventAdd={handleEventAdd}
+      />
     </div>
   );
 }
