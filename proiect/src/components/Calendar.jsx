201cd41e6bb14ccd27a2b4035f492f3e2e607636
DariusPvl3 <paval.darius26@gmail.com>
2023-05-24 22:48:11 +0300

    preparing for database inserting

--- proiect/src/components/Calendar.jsx
+++ proiect/src/components/Calendar.jsx
@@ -1,29 +1,86 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import axios from "axios";
 
 function Calendar() {
   const calendarRef = useRef(null);
   const [calendarEvents, setCalendarEvents] = useState([]);
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    // Fetch events from MongoDB database and set them in calendarEvents state
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get("/api/events");
+      setCalendarEvents(response.data);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  const addEventToDatabase = async (event) => {
+    try {
+      await axios.post("/api/events", event);
+    } catch (error) {
+      console.error("Error adding event:", error);
+    }
+  };
+
+  const events = [
+    {
+      title: "Test",
+      start: "2023-05-24",
+      end: "2023-05-24",
+      location: "Event Location",
+      category: "Event Category",
+    },
+  ];
 
   const handleEventAdd = ({ event }) => {
     if (event && event.title && event.start) {
       setCalendarEvents([...calendarEvents, event]);
+      addEventToDatabase(event);
     }
   };
 
   const handleDateSelect = (selectInfo) => {
+    setTitle("");
+    setLocation("");
+    setCategory("");
+
     const title = prompt("Enter event title:");
     if (title) {
-      const calendarApi = selectInfo.view.calendar;
-      calendarApi.addEvent({
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
+      setTitle(title);
+
+      const location = prompt("Enter event location:");
+      if (location) {
+        setLocation(location);
+
+        const category = prompt(
+          "Enter event category (Game Night, Meeting, Party, Team-Building, Vacation):"
+        );
+        if (category) {
+          setCategory(category);
+
+          const calendarApi = selectInfo.view.calendar;
+          calendarApi.addEvent({
+            title,
+            start: selectInfo.startStr,
+            end: selectInfo.endStr,
+            allDay: selectInfo.allDay,
+            location,
+            category,
+          });
+        }
+      }
     }
   };
 
@@ -86,7 +143,7 @@ function Calendar() {
           end: "customTimeGridDayButton customTimeGridWeekButton customDayGridMonthButton",
         }}
         height={"90vh"}
-        events={calendarEvents}
+        events={events}
         selectable={true}
         selectMirror={true}
         dayMaxEvents={true}
--- proiect/src/components/Filters.jsx
+++ proiect/src/components/Filters.jsx
@@ -4,8 +4,10 @@ function Filters(){
     return (
         <div>
             Game Night<input type="checkbox" name="game-night" id="game-night" checked/><br></br>
-            Party<input type="checkbox" name="game-night" id="game-night" checked="true"/><br></br>
-            Team-Building<input type="checkbox" name="game-night" id="game-night" checked="true"/><br></br>
+            Meeting<input type="checkbox" name="meeting" id="meeting" checked="true"/><br></br>
+            Party<input type="checkbox" name="party" id="party" checked="true"/><br></br>
+            Team-Building<input type="checkbox" name="team-building" id="team-building" checked="true"/><br></br>
+            Vacation<input type="checkbox" name="vacation" id="vacation" checked="true"/><br></br>
         </div>
     );
 }
